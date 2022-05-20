210a46e5a91a330566ec8014a3bfef98089ddb6c
Maria Tanasă <mariatanasa2000@gmail.com>
2022-05-20 19:30:56 +0300

    Testare

---
+++ Seminar12/persoana/clase/IPersoana.java
@@ -0,0 +1,9 @@
+package persoana.clase;
+
+public interface IPersoana {
+    public String getSex();
+
+    public int getVarsta();
+
+    public boolean checkCNP();
+}
---
+++ Seminar12/persoana/exceptii/CNPInvalidException.java
@@ -0,0 +1,4 @@
+package persoana.exceptii;
+
+public class CNPInvalidException extends IllegalArgumentException{
+}
